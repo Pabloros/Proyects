9cc27688a29408ffb1debc8e7336720ab446b1c3
Pabloros <yoopigr09@gmail.com>
2018-08-16 11:41:31 -0500

    first commit

---
+++ enviar.php
@@ -0,0 +1,14 @@
+<¿php
+      $destino = ("iron_0002@hotmail.com")
+      $name = $_POST ["name"];
+      $emali = $_POST ["email"];
+      $messsage = $_POST ["message"];
+      $contenido("nombre: " . $name . $email <br/> "escribió: <br/>" . $message );
+      mail(destino);
+
+      if(("iron_0002@hotmail.com")) {
+      echo: "Gracis por tu mensaje"
+      }else{
+      echo: "uyyyy algo salió mal, intentalo más tarde"
+      }
+¿>
